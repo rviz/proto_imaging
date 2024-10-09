cea1f303646f3e34b07378dc8937c927dcf3f0b7
rviz <88725943+rviz@users.noreply.github.com>
2024-10-09 15:16:21 -0600

    diseño preliminar

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,15 @@ import ModelViewer from "../components/modelviewer";
 export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center bg-transparent">
-      <h1>3D Model Viewer</h1>
+      <h1 className="mt-10 text-3xl">Virtual Reality</h1>
       <ModelViewer fbxUrl="/models/modelone.fbx" />
+      <a
+        href="https://view.genially.com/669c0d9d9c64fa04779654a2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="mb-10 text-3xl">Iniciar</button>
+      </a>
     </div>
   );
 }
