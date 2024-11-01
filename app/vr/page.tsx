568392ed7fe9559c9c87e1b2ea6aa16b2ea34e85
rviz <88725943+rviz@users.noreply.github.com>
2024-11-01 16:17:44 -0600

    AI agregado preliminar

---
+++ app/vr/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import ModelViewer from "../../components/modelviewer";
+
+export default function Vr() {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center bg-slate-200">
+      <div className="z-10 top-[2%] absolute w-3/5 flex flex-col items-center justify-center bg-transparent  p-10 rounded-lg">
+        <h1 className="text-[150px] text-geniallyblue-100 font-extrabold">
+          VIRTUAL
+        </h1>
+        <h1 className="absolute mt-[300px] text-[150px] text-geniallyblue-100 font-bold">
+          REALITY
+        </h1>
+      </div>
+
+      <ModelViewer fbxUrl="/models/modelvr.fbx" />
+
+      <a
+        className="z-10 bottom-[7%] absolute  group inline-block rounded bg-gradient-to-r from-cyan-500 via-indigo-500 to-blue-500 p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75"
+        href="https://view.genially.com/669c0d9d9c64fa04779654a2"
+      >
+        <span className="block rounded-sm bg-geniallyblue-100 px-24 py-10 text-[40px] group-hover:bg-transparent text-white font-bold">
+          Click aquí para iniciar
+        </span>
+      </a>
+    </div>
+  );
+}
---
+++ components/modelviewerAI.js
@@ -0,0 +1,53 @@
+"use client";
+import React, { Suspense, useEffect, useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { FBXLoader } from "three-stdlib";
+
+const ModelViewer = ({ fbxUrl }) => {
+  const modelRef = useRef();
+  console.log("ModelViewer is rendering with fbxUrl:", fbxUrl);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const loader = new FBXLoader();
+
+      console.log("Starting to load FBX model...");
+
+      loader.load(
+        fbxUrl,
+        (fbx) => {
+          console.log("FBX model loaded:", fbx);
+          fbx.scale.set(1, 1, 1);
+
+          modelRef.current.add(fbx);
+        },
+        undefined,
+        (error) => {
+          console.error("Error loading FBX model:", error);
+        }
+      );
+    }, 100); // Adjust time as necessary
+
+    return () => clearTimeout(timer); // Cleanup the timer
+  }, [fbxUrl]);
+
+  return (
+    <Canvas camera={{ position: [2, 0, 10], fov: 105 }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} />
+
+      <Suspense fallback={<div>Loading model...</div>}>
+        <group
+          ref={modelRef}
+          scale={[0.07, 0.07, 0.07]}
+          position={[0, -5, 0]}
+        />
+      </Suspense>
+
+      <OrbitControls />
+    </Canvas>
+  );
+};
+
+export default ModelViewer;
